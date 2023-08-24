3bf0d453b2b56490fcd4269e904cbc10bd5c4f7b
Harris Basra <harrisbasra@gmail.com>
2023-08-24 20:13:18 +0500

    Initial commit

---
+++ lib/pages/splash_screen00.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+
+
+class SplashScreen extends StatefulWidget {
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState(){
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen.withScreenFunction(
+      splash: Center(
+        child: Image.asset('assets/images/img.png'),
+      ),
+      screenFunction: () async {
+        return start_page();
+      },
+      duration: 1700,
+      splashIconSize: 10000.0,
+      splashTransition: SplashTransition.fadeTransition,
+      // pageTransitionType: PageTransitionType.rightToLeftWithFade,
+      animationDuration: Duration(seconds: 1),
+    );
+  }
+}
